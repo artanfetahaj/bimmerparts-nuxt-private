54b8e7f31446bb4113a6dcd01ccc18a5d6b1cf4e
FlorentAzemi <florentazemi27@gmail.com>
2026-05-19 14:45:17 +0200

    removed navbar from register page

--- app/pages/account/register.vue
+++ app/pages/account/register.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import authService from '@/services/auth'
-
+definePageMeta({ layout: 'blank' })
 if (import.meta.client && authService.isAuthenticated()) {
   await navigateTo('/account', { replace: true })
 }
