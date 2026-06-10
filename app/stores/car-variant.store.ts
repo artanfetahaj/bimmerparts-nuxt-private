8333622b4bedda8d18ba085990742b39cf06487d
FlorentAzemi <florentazemi27@gmail.com>
2026-06-10 10:21:15 +0200

    car variant optional in car filter selection

--- app/stores/car-variant.store.ts
+++ app/stores/car-variant.store.ts
@@ -1,10 +1,12 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import type { CarVariant } from '@/models/CarVariant'
+import type { CarModel } from '@/models/CarModel'
 
 export const useCarVariantStore = defineStore('carVariant', () => {
   const isDialogOpen = ref(false)
   const selectedVariant = ref<CarVariant | null>(null)
+  const selectedModel = ref<CarModel | null>(null)
 
   function openDialog() {
     isDialogOpen.value = true
@@ -15,21 +17,30 @@ export const useCarVariantStore = defineStore('carVariant', () => {
   }
 
   function setVariant(variant: CarVariant) {
-    selectedVariant.value = variant;
+    selectedVariant.value = variant
+    selectedModel.value = null
+    closeDialog()
+  }
 
+  function setModel(model: CarModel) {
+    selectedModel.value = model
+    selectedVariant.value = null
     closeDialog()
   }
 
   function clearVariant() {
     selectedVariant.value = null
+    selectedModel.value = null
   }
 
   return {
     isDialogOpen,
     selectedVariant,
+    selectedModel,
     openDialog,
     closeDialog,
     setVariant,
+    setModel,
     clearVariant,
   }
 })
